187d7eb56f9e8e0c660947bdcd87c394f169a126
Craig Kahle <craig.kahle@austin.utexas.edu>
2014-01-08 16:44:20 -0600

    Meteor is pretty cool...

---
+++ newApp.js
@@ -0,0 +1,27 @@
+if (Meteor.isClient) {
+  Template.hello.greeting = function () {
+    return "Welcome to newApp. Nice work. It updates on its own WTF";
+  };
+
+  Template.hello.events({
+    'click input' : function () {
+      // template data, if any, is available in 'this'
+      if (typeof console !== 'undefined')
+        console.log("You pressed the button");
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
+
+Musicians = new Meteor.Collection("musicians");
+
+if (Meteor.is_client) {
+  Template.musician_list.Musicians = function () {
+    return Musicians.find({}, {sort: {likes: -1, name: 1}});
+  };
+}
