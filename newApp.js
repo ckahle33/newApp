42d09cd38eb62af90551c97430d1e7fc51428a97
Craig Kahle <craig.kahle@austin.utexas.edu>
2014-01-08 16:56:10 -0600

    message

--- newApp.js
+++ newApp.js
@@ -1,6 +1,6 @@
 if (Meteor.isClient) {
   Template.hello.greeting = function () {
-    return "Welcome to newApp. Nice work. It updates on its own WTF";
+    return "Welcome to musician list bruh. Nice work. It updates on its own WTF";
   };
 
   Template.hello.events({
